2de23b4c0be7ef5eaf0ed5717ee717a4b39993d0
Tony Deng <h.tony.deng@gmail.com>
2021-09-11 20:18:09 -0600

    updating server for use with client

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import json
+from nltk.corpus import wordnet
+from flask import Flask, jsonify
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import ast
@@ -12,8 +14,6 @@ EXPLAIN = True
 # nltk.download('wordnet')
 # from nltk.corpus import stopwords
 # from nltk.tokenize import word_tokenize
-from nltk.corpus import wordnet
-import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -21,20 +21,20 @@ api = Api(app)
 # learned from https://towardsdatascience.com/the-right-way-to-build-an-api-with-python-cd08ab285f8f
 
 
-#compare all the synsets of each pair of a word in w1(event) and w2(scenario)
+# compare all the synsets of each pair of a word in w1(event) and w2(scenario)
 def word_similiarity(w1, w2):
     sl1 = wordnet.synsets(w1)
     sl2 = wordnet.synsets(w2)
     max_score = 0
     if EXPLAIN:
-        best_matching_word={
-            'x':"",
-            'y':"",
-            'score':0
+        best_matching_word = {
+            'x': "",
+            'y': "",
+            'score': 0
         }
     for x in sl1:
         for y in sl2:
-#           Use WUP similarity algorithm here
+            #           Use WUP similarity algorithm here
             score = x.wup_similarity(y)
             max_score = max(max_score, score if score else 0)
             if EXPLAIN and score == max_score:
@@ -42,59 +42,83 @@ def word_similiarity(w1, w2):
                 best_matching_word['y'] = y.name().split('.')[0]
                 best_matching_word['score'] = score
     result = (max_score,)
-    if EXPLAIN: result += (f'"{w1}" -> "'+ best_matching_word['x']+ f'" < match with > "{w2}" -> "'+ best_matching_word['y']+ f'" | matching score = '+ str(best_matching_word['score']),)
+    if EXPLAIN:
+        result += (f'"{w1}" -> "' + best_matching_word['x'] + f'" < match with > "{w2}" -> "' +
+                   best_matching_word['y'] + f'" | matching score = ' + str(best_matching_word['score']),)
     return result
 
 # Return cosine similiarities between 2 sentences
-def check_2_sentence_similarities(X, Y, USE_WORDNET_SIMILARITY = True):
+
+
+def check_2_sentence_similarities(X, Y, USE_WORDNET_SIMILARITY=True):
     print(f'--- compare between event "{X}" and scenario "{Y}" ---')
-    
+
     # tokenization and normalization
     X_list = [w.lower() for w in word_tokenize(X)]
     Y_list = [w.lower() for w in word_tokenize(Y)]
 
     # sw contains the list of stopwords
-    sw = stopwords.words('english') 
-    l1 =[];l2 =[]
+    sw = stopwords.words('english')
+    l1 = []
+    l2 = []
 
     # remove stop words from the string
-    X_set = {w for w in X_list if not w in sw} 
+    X_set = {w for w in X_list if not w in sw}
     Y_set = {w for w in Y_list if not w in sw}
 
-    # form a set containing keywords of both strings 
-    rvector = X_set.union(Y_set) 
-    if EXPLAIN: print(f' axis names of the combined words\'s vector space: {rvector}')
-    if USE_WORDNET_SIMILARITY: 
+    # form a set containing keywords of both strings
+    rvector = X_set.union(Y_set)
+    if EXPLAIN:
+        print(f' axis names of the combined words\'s vector space: {rvector}')
+    if USE_WORDNET_SIMILARITY:
         for w in rvector:
-            X_set_match = max([word_similiarity(w,X_set_word) for X_set_word in X_set])
+            X_set_match = max([word_similiarity(w, X_set_word)
+                               for X_set_word in X_set])
             X_set_scores = X_set_match[0]
-            if EXPLAIN: print(f' → word in event: "{w}", analysis | ', X_set_match[1])
-            if X_set_scores: l1.append(X_set_scores) # create a vector
-            else: l1.append(0)
-                
-            Y_set_match = max([word_similiarity(w,Y_set_word) for Y_set_word in Y_set])
+            if EXPLAIN:
+                print(f' → word in event: "{w}", analysis | ', X_set_match[1])
+            if X_set_scores:
+                l1.append(X_set_scores)  # create a vector
+            else:
+                l1.append(0)
+
+            Y_set_match = max([word_similiarity(w, Y_set_word)
+                               for Y_set_word in Y_set])
             Y_set_scores = Y_set_match[0]
-            if EXPLAIN: print(f' → word in scenario: "{w}", analysis | ', Y_set_match[1])
-            if Y_set_scores: l2.append(Y_set_scores)
-            else: l2.append(0)
+            if EXPLAIN:
+                print(
+                    f' → word in scenario: "{w}", analysis | ', Y_set_match[1])
+            if Y_set_scores:
+                l2.append(Y_set_scores)
+            else:
+                l2.append(0)
     else:
         for w in rvector:
-            if w in X_set: l1.append(1) # create a vector
-            else: l1.append(0)
-            if EXPLAIN: print(f' → word in event: "{w}", analysis | ', l1[-1])
-            if w in Y_set: l2.append(1)
-            else: l2.append(0)
-            if EXPLAIN: print(f' → word in scenario: "{w}", analysis | ', l2[-1])
-#     
-    if EXPLAIN: print(f' event {X_set} makes vector: {l1} and scenario {Y_set} makes vector: {l2}')
+            if w in X_set:
+                l1.append(1)  # create a vector
+            else:
+                l1.append(0)
+            if EXPLAIN:
+                print(f' → word in event: "{w}", analysis | ', l1[-1])
+            if w in Y_set:
+                l2.append(1)
+            else:
+                l2.append(0)
+            if EXPLAIN:
+                print(f' → word in scenario: "{w}", analysis | ', l2[-1])
+#
+    if EXPLAIN:
+        print(
+            f' event {X_set} makes vector: {l1} and scenario {Y_set} makes vector: {l2}')
     c = 0
 
-    # cosine formula 
+    # cosine formula
     for i in range(len(rvector)):
         c += l1[i]*l2[i]
-    #use max(,0.001) is for the case where there is 0 match meaning between the 2 sentences
-    cosine = c / max(float((sum(l1)*sum(l2))**0.5),0.001)
-    print(f' similarity between event "{X}" and scenario "{Y}": ', '\033[1m' + str(cosine) + '\033[0m')
+    # use max(,0.001) is for the case where there is 0 match meaning between the 2 sentences
+    cosine = c / max(float((sum(l1)*sum(l2))**0.5), 0.001)
+    print(f' similarity between event "{X}" and scenario "{Y}": ',
+          '\033[1m' + str(cosine) + '\033[0m')
     return cosine
 
 
@@ -107,8 +131,11 @@ sp_df = pd.read_csv('principles.csv')
 # def find_principles(event, sp_pairs):
 #     sorted_sp_paris = sorted([(check_2_sentence_similarities(event, senario), principle) for senario, principle in sp_pairs.items()])[::-1]
 #     return [pair[1] for pair in sorted_sp_paris if pair[0]][:5]
+
+
 def find_principles(event, sp_df):
-    sorted_sp_paris = sorted([(check_2_sentence_similarities(event, row['scenario']), row['principle']) for idx, row in sp_df.iterrows()])[::-1]
+    sorted_sp_paris = sorted([(check_2_sentence_similarities(
+        event, row['scenario']), row['principle']) for idx, row in sp_df.iterrows()])[::-1]
     return [pair[1] for pair in sorted_sp_paris if pair[0]][:5]
 
 # sp_pairs = {
@@ -117,7 +144,7 @@ def find_principles(event, sp_df):
 #                 "eat breakfast": "example principle for eat breakfast",
 #                 "excersize": "example principle for excersize",
 #                 "angry": "example principle for angry",
-#                 "software debugging": "example principle for software debugging", 
+#                 "software debugging": "example principle for software debugging",
 #                 "Brainstorming Session": "example principle for Brainstorming Session"
 #            }
 
@@ -137,18 +164,21 @@ class Users(Resource):
     def get(self):
         # data = pd.read_csv('users.csv')  # read CSV
         # data = data.to_dict()  # convert dataframe to dictionary
-        
+
         # return {'data': data}, 200  # return data and 200 OK code
-        return {'hello':"world"}, 200
+        response = jsonify({'hello': "world"})
+        response.status_code = 200
+        return response
+
     def post(self):
         parser = reqparse.RequestParser()  # initialize
-        
+
         parser.add_argument('userId', required=True)  # add args
         parser.add_argument('event', required=True)
         # parser.add_argument('city', required=True)
-        
+
         args = parser.parse_args()  # parse arguments to dictionary
-        
+
         # create new dataframe containing new values
         # new_data = pd.DataFrame({
         #     'userId': args['userId'],
@@ -156,7 +186,6 @@ class Users(Resource):
         #     'city': "waterloo",
         #     'locations': [[]]
         # })
-
 
         # import pdb; pdb.set_trace()
         # read our CSV
@@ -167,11 +196,16 @@ class Users(Resource):
         # data.to_csv('users.csv', index=False)
         event = args['event']
         # print(f'*** most recommended principles found for event \"{event}\" are {find_principles(event, sp_pairs)} ***')
-        return {'data': f'most recommended principles found for event \"{event}\" are {find_principles(event, sp_df)} '}, 200  # return data with 200 OK
+        # return data with 200 OK
+        response = jsonify(
+            {'event': event, 'principles': find_principles(event, sp_df)})
+        response.status_code = 200
+        return response
         # return {'data':f'{new_data['event'].tolist()}' }, 200  # return data with 200 OK
         # return {'data':f'{event}' }, 200
+
 
 api.add_resource(Users, '/users')  # '/users' is our entry point
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8078)  # run our Flask app
+    app.run(host='0.0.0.0', port=5000)  # run our Flask app

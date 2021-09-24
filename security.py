2365cd9a77fd4bf62ecfabd5741cba62602a1e2c
Tony Deng <h.tony.deng@gmail.com>
2021-09-23 21:13:53 -0600

    setting up nlp and crypt client

---
+++ security.py
@@ -0,0 +1,11 @@
+from cryptography.hazmat.primitives import serialization
+import jwt
+
+
+class SecurityClient:
+    def __init__(self, private_key, password):
+        self.key = serialization.load_ssh_private_key(
+            open(private_key, 'r').read().encode(), password=password.encode())
+
+    def encryptPayload(self, payload):
+        return jwt.encode(payload=payload, key=self.key, algorithm='RS256')

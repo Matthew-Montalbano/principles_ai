ead0e77844ae3120ca6569994e0fcf1d457568fd
ti.guo <guoty3310@gmail.com>
2021-10-15 18:35:29 -0400

    add openai api and result parsing

--- nlp.py
+++ nlp.py
@@ -4,6 +4,8 @@ from nltk.corpus import wordnet
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import pandas as pd
+import openai
+from decouple import config
 
 EXPLAIN = False
 
@@ -116,7 +118,105 @@ class UpNlpClient:
         return cosine
 
     def find_principles(self, event):
+        #TODO: need to delete this code at the end
         sorted_sp_paris = sorted([(self.check_2_sentence_similarities(
             event, row['scenario']), row['principle']) for idx, row in self.sp_df.iterrows()])[::-1]
         results = [pair[1] for pair in sorted_sp_paris if pair[0]][:3]
         return results
+
+    def gpt3_classify_event(self, 
+        event_title,
+        event_location = "N/A",
+        event_participants = "N/A",
+        event_time = "2021, Sep 25, 4pm",
+        event_duration = ""):
+
+        #TODO: add this to private key
+        openai.api_key = config('OPENAI_KEY')
+
+        for number_of_try in range(10):
+            print(f"number { number_of_try} try for openAI api request")
+            # prompt = f"Event: \"Daily Standup\"\nEvent Location: \"Room 203\"\nEvent Time: \"2021, Sep 20, 9am\"\nEvent Participants: \"robert@ibm.com; amy@ibm.com; \"\nresult: work,indoor,offline,participate in meeting\n###\nEvent: \"Submit project solution design for GM\"\nEvent Location: \"https://zoom.us/j/94854324867?pwd=L0NlaWpaQzhjSmk1WlVnK05QT1M5UT09\"\nEvent Time: \"2021, Sep 21, 4pm\"\nEvent Participants: \"bob@apple.com; sally@ibm.com; \"\nresult: work,indoor,online,submit project\n###\nEvent: \"Shang-Chi Movie Night\"\nEvent Location: \"Galaxy Cinemas Waterloo\"\nEvent Time: \"2021, Sep 22, 9pm\"\nEvent Participants: \"guoty3310@gmail.com; pp123@hotmail.com; \"\nresult: life,indoor,offline,watch movie\n###\nEvent: \"Lunch\"\nEvent Location: \"N/A\"\nEvent Time: \"2021, Sep 24, 12pm\"\nEvent Participants: \"N/A\"\nresult: life,unknown,offline,eat lunch\n###\nEvent: \"{event_title}\"\nEvent Location: \"{event_location}\"\nEvent Time: \"{event_time}\"\n{event_participants}: \"N/A\"\nresult: "
+            # NOTE: in the prompt, there should be no spaces between tags
+            response = openai.Completion.create(
+                engine="davinci",
+                prompt=f"Event: \"Daily Standup\"\nEvent Location: \"Room 203\"\nEvent Time: \"2021, Sep 20, 9am\"\nEvent Participants: \"robert@ibm.com; amy@ibm.com; \"\nresult:work,indoor,offline,participate in meeting\n###\nEvent: \"Submit project solution design for GM\"\nEvent Location: \"https://zoom.us/j/94854324867?pwd=L0NlaWpaQzhjSmk1WlVnK05QT1M5UT09\"\nEvent Time: \"2021, Sep 21, 4pm\"\nEvent Participants: \"bob@apple.com; sally@ibm.com; \"\nresult:work,indoor,online,submit project\n###\nEvent: \"Shang-Chi Movie Night\"\nEvent Location: \"Galaxy Cinemas Waterloo\"\nEvent Time: \"2021, Sep 22, 9pm\"\nEvent Participants: \"guoty3310@gmail.com; pp123@hotmail.com; \"\nresult:life,indoor,offline,watch movie\n###\nEvent: \"Lunch\"\nEvent Location: \"N/A\"\nEvent Time: \"2021, Sep 24, 12pm\"\nEvent Participants: \"N/A\"\nresult:life,unknown,offline,eat lunch\n###\nEvent: \"{event_title}\"\nEvent Location: \"{event_location}\"\nEvent Time: \"{event_time}\"\nEvent Participants: \"{event_participants}\"\nresult:",
+                temperature=0,
+                max_tokens=20,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                stop=["###"]
+                )
+            #TODO: find out if there could ever be a response['choices'][0]['text']
+            text = response['choices'][0]['text']
+            if len(text) > 10:
+                print(f'OpenAI reurned text: \n ------begin------\n{text}\n------end------')
+                end_idx = text.find('\n')
+                comma_idx = text.find(",")
+                start_idx = comma_idx-4 #TODO: improve this logic to include the cases not 4
+                return text[start_idx:end_idx]
+                
+        return ""
+
+    def gpt3_find_matching_scenarios(self, event, scenarios_df):
+        # Get predictions
+        event_values = [v for v in event.values()]
+        predictions = self.gpt3_classify_event(event_values)
+        # predictions = 'work,group,offline,go to team social' #TODO: delete this to use the real prediction
+        if not predictions:
+            return []
+        prediction_list = predictions.split(',')
+        event_summary = prediction_list[-1]
+        event_tags = prediction_list[:-1]
+        
+        # find matches 
+        scenarios_df['matching_score'] = 0
+        for i, row in scenarios_df.iterrows():
+            #TODO: convert the event_tags and row['tags'] to set will speed this up
+            score = len([word for word in row['tags'] if word in event_tags])
+            scenarios_df.at[i,'matching_score'] = score
+        
+        scenarios_df = scenarios_df[scenarios_df['matching_score'] !=0]
+        top3_df = scenarios_df.sort_values(['matching_score'], ascending=[False]).head(3)
+        print(top3_df)
+        # return top3_df['scenarios'].tolist()
+        
+        principle_scenario_pairs_df = pd.DataFrame(columns = ["principles", "scenarios"])
+        principle_scenario_pairs_df["scenarios"] = []
+        # make principle_scenario_pairs, the scencario here will be useful for formatting principles later
+        for i, row in top3_df.iterrows():
+            row_to_add = {
+                "principles": row['principles'][0],
+                "scenarios": row['scenarios']
+            }
+            principle_scenario_pairs_df = principle_scenario_pairs_df.append(row_to_add, ignore_index = True)
+            # for principle_id in row['principles']:
+            #     import pdb; pdb.set_trace()
+            #     if principle_id not in principle_scenario_pairs_df['principles']:
+            #         row_to_add = {
+            #             "principles": principle_id,
+            #             "scenarios": [row['scenarios']]
+            #         }
+            #         principle_scenario_pairs_df = principle_scenario_pairs_df.append(row_to_add, ignore_index = True)
+            #     else:
+            #         principle_scenario_pairs_df.loc[principle_scenario_pairs_df['principles'] == principle_id, 'scenarios'] += [row['scenarios']]
+                
+        print(f'principle_scenario_pairs: {principle_scenario_pairs_df}')
+
+        return principle_scenario_pairs_df 
+
+    def format_principles(self, principles_df, principle_scenario_pairs_df):
+        results = []
+        for i, row in principles_df.iterrows():
+            # import pdb; pdb.set_trace()
+            scenario = principle_scenario_pairs_df.loc[principle_scenario_pairs_df['principles'] == row['id'], 'scenarios'].tolist()[0]
+            problem = f'👀 {scenario}?'
+            principle = row['principle'].replace("\n", "")
+            reminder = f'💡 Reminder: {principle}'
+            notes = '🔎  ' + row['notes']
+            source = '🔗 By: ' + row['source']
+            seperator = "-----"
+            results.append('\n'.join([seperator, problem, reminder, notes, source]))
+        return results
+        
